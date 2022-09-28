f89a694b4775f0e4becf3cc276bcdc75dceeee3f
Gastón Montiel <105525139+Heldmar27@users.noreply.github.com>
2022-09-28 16:50:45 -0300

    Add files via upload

---
+++ Nueva carpeta (2)/Lab2Tp8/RaizScanner.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RaizScanner {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //Inicializo la clase Scanner y declaro mis variables.
+        Scanner entrada = new Scanner(System.in);
+        int numeroR;
+
+        //Le pido al usuario ingresar un número.
+        System.out.println("Ingrese un número: ");
+        numeroR = entrada.nextInt();
+
+        //Imprimo el número y su raíz por consola.
+        System.out.println("la raíz de "+numeroR+" es: "+Math.sqrt(numeroR));
+
+
+
+    }
+}
